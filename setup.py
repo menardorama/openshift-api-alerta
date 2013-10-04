59f9081c42d453fbce6ce9602e5d9b1222ef502f
Nick Satterly <nick.satterly@guardian.co.uk>
2013-10-04 16:28:07 +0100

    get alerta script working

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ setuptools.setup(
     author_email='nick.satterly@theguardian.com',
     url='http://github.com/guardian/alerta',
     license='Apache License 2.0',
+    packages=setuptools.find_packages(exclude=['bin', 'tests']),
     include_package_data=True,
+    zip_safe=False,
     install_requires=[
         'Flask',
         'pymongo',
@@ -17,8 +19,10 @@ setuptools.setup(
         'PyYAML',
         'pytz'
     ],
-    scripts=[
-        'bin/alerta',
-    ],
+    entry_points={
+        'console_scripts': [
+            'alerta = wsgi.alerta.server.shell:main',
+        ]
+    },
     keywords='alert monitoring system'
 )
