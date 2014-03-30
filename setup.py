dd8cb88dfd5382a9b332cfca38b3f4ab7257d5de
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-31 00:14:49 +0100

    ensure 3.0.1 is deployed

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'alerta'
+        'alerta==3.0.1'
     ],
     keywords='alert monitoring system openshift paas'
 )
