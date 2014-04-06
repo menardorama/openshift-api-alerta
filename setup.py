76a9c1e5e37a0e90e8391e936a17f1de24f87aa2
Nick Satterly <nick.satterly@guardian.co.uk>
2014-04-06 09:59:24 +0100

    always install the latest version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='openshift-api-alerta',
-    version='3.0.1',
+    version='3.0',
     description='Alerta monitoring framework on OpenShift',
     author='Nick Satterly',
     author_email='nick.satterly@theguardian.com',
@@ -13,7 +13,7 @@ setuptools.setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'alerta==3.0.1'
+        'alerta'
     ],
     keywords='alert monitoring system openshift paas'
 )
