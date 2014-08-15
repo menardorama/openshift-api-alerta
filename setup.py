8111015d813b0441cd9b13f5d788a131e5df1a05
Nick Satterly <nick.satterly@guardian.co.uk>
2014-08-15 14:39:51 +0100

    update to release 3.2

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ import setuptools
 
 setuptools.setup(
     name='openshift-api-alerta',
-    version='3.1.0',
-    description='Alerta monitoring framework on OpenShift',
+    version='3.2.4',
+    description='Alerta API on OpenShift',
     author='Nick Satterly',
     author_email='nick.satterly@theguardian.com',
     url='http://github.com/guardian/alerta',
     license='Apache License 2.0',
-    packages=setuptools.find_packages(exclude=['bin', 'tests']),
-    include_package_data=True,
-    zip_safe=False,
     install_requires=[
-        'alerta-app==3.1.0'
+        'alerta-server==3.2.4',
+        'alerta==3.2.4'
     ],
-    dependency_links = [
-        'https://github.com/guardian/alerta/tarball/feature/login-oauth#egg=alerta-app-3.1.0'
+    dependency_links=[
+        'https://github.com/guardian/alerta/tarball/release/3.2#egg=alerta-server-3.2.4',
+        'https://github.com/alerta/python-alerta-client/tarball/master#egg=alerta-3.2.4'
     ],
     keywords='alert monitoring system openshift paas'
 )
---
+++ wsgi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+os.environ['ALERTA_SVR_CONFIG_FILE'] = os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'alertad.conf')
+
+from alerta.app import app as application
+
