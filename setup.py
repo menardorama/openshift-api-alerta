ee5ca193d07ad8e7b759b9174bc256b3ce8b72f9
Nick Satterly <nick.satterly@guardian.co.uk>
2014-12-17 14:35:16 +0000

    bump version to 3.3.1

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 
 import setuptools
 
+ALERTA_SERVER_VERSION = '3.3.1'
+ALERTA_CLIENT_VERSION = '3.2.9'
+
 setuptools.setup(
     name='openshift-api-alerta',
-    version='3.2.4',
+    version='%s' % ALERTA_SERVER_VERSION,
     description='Alerta API on OpenShift',
     author='Nick Satterly',
     author_email='nick.satterly@theguardian.com',
     url='http://github.com/guardian/alerta',
     license='Apache License 2.0',
     install_requires=[
-        'alerta-server==3.2.6',
-        'alerta==3.2.6'
+        'alerta-server==%s' % ALERTA_SERVER_VERSION,
+        'alerta==%s' % ALERTA_CLIENT_VERSION
     ],
     dependency_links=[
-        'https://github.com/guardian/alerta/tarball/release/3.2#egg=alerta-server-3.2.6',
-        'https://github.com/alerta/python-alerta-client/tarball/master#egg=alerta-3.2.6'
+        'https://github.com/guardian/alerta/tarball/master#egg=alerta-server-%s' % ALERTA_SERVER_VERSION,
+        'https://github.com/alerta/python-alerta-client/tarball/master#egg=alerta-%s' % ALERTA_CLIENT_VERSION
     ],
     keywords='alert monitoring system openshift paas'
 )
